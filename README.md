8f2ef4887fe6f65307b879dd0026d91dd82017d9
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-10-08 20:37:02 +0100

    feat: application logic

--- README.md
+++ README.md
@@ -12,11 +12,10 @@ curl -H "Content-type: application/json" -d '[['ATL', 'EWR'], ['SFO', 'ATL']]' '
 
 ## Schema
 
-```
+```json
 [['SFO''EWR']]  => ['SFO', 'EWR']
 [['ATL', 'EWR'], ['SFO', 'ATL']]     => ['SFO', 'EWR']
 [['IND', 'EWR'], ['SFO', 'ATL'], ['GSO', 'IND'], ['ATL', 'GSO']] => ['SFO', 'EWR']
-
 ```
 
 ## Installation
@@ -25,4 +24,6 @@ Assuming you have the rust toolchain installed with `cargo`: `cargo run`
 
 ## Estimation
 
-My rust is only so-so I recently started getting serious with it about a week or two ago, I have never used rust on the web, I had to learn actix, futures/async and ownership/lifetime of a graph data structure in rust on the fly.
+My rust is only so-so I recently started getting serious with it about a week or two ago, I have never used rust on the web.
+Took about six hours. Including tests and documentation. The solution itself was not hard to come up with, navigating some of the idiosyncracies of
+actix took up most of the time.
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    get, web, error, Result, Responder,
+    http::{header::ContentType, StatusCode},
+     middleware::Logger,
+    App, HttpResponse, HttpServer,
+};
+use std::collections::HashMap;
+use serde::Deserialize;
+use log::info;
+use derive_more::{Display, Error};
+
 
 #[get("/")]
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Flight path tracker microservice. Responds only to POST /calculate")
 }
 
+#[rustfmt::skip]
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+        std::env::set_var("RUST_LOG", "info");
+    std::env::set_var("RUST_BACKTRACE", "1");
+    env_logger::init();
+
     HttpServer::new(|| {
+        let logger = Logger::default();
+
         App::new()
+            .wrap(logger)
             .service(index)
             .route("/calculate", web::post().to(flight_path))
     })
@@ -17,8 +35,55 @@ async fn main() -> std::io::Result<()> {
     .await
 }
 
-async fn flight_path() -> impl Responder {
-    // TODO:  validate req body
-    // TODO: flight path logic
-    HttpResponse::Ok().body("Hey there!")
+async fn flight_path(tickets: web::Json<Vec<(String, String)>>) -> impl Responder {
+
+    println!("{:?}", ticket_route(vec![Ticket(String::from("SFO"), String::from("EWR"))]));
+    HttpResponse::Ok().body("pass")
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Ticket(String, String);
+
+pub fn ticket_route(tickets: Vec<Ticket>) -> Result<Ticket, TicketError> {
+    let mut ticket_path = HashMap::new();
+
+    // O(n) time complexity
+    for ticket in tickets {
+        let source = ticket.0;
+        let destination = ticket.1;
+
+        *ticket_path.entry(source).or_insert(0) += 1;
+        *ticket_path.entry(destination).or_insert(0) -= 1;
+    }
+
+    let search_source = find_key_for_value(&ticket_path, 1);
+    let search_destination = find_key_for_value(&ticket_path, -1);
+
+    match (search_source, search_destination) {
+        (Some(s), Some(d)) => Ok(Ticket(s.to_string(), d.to_string())),
+        _ => Err(TicketError::ValidationError{field: "no flight path found".to_string()})
+    }
+}
+
+fn find_key_for_value<'a>(map: &'a HashMap<String, i32>, value: i32) -> Option<&'a String> {
+    map.iter().find_map(|(key, &val)| if val == value { Some(key) } else { None })
+}
+
+#[derive(Debug, Display, Error)]
+pub enum TicketError {
+    #[display(fmt = "Validation error on field: {}", field)]
+    ValidationError { field: String },
+}
+
+impl error::ResponseError for TicketError {
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::build(self.status_code())
+            .insert_header(ContentType::html())
+            .body(self.to_string())
+    }
+    fn status_code(&self) -> StatusCode {
+        match *self {
+            TicketError::ValidationError { .. } => StatusCode::BAD_REQUEST,
+        }
+    }
 }
